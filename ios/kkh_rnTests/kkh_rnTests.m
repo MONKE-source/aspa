5b011089313fcb059b8adabc03428051afc23bd2
Arth Aggarwal <arth_aggarwal@s2022.ssts.edu.sg>
2024-08-07 20:52:26 +0800

    renamed

--- ios/kkh_rnTests/kkh_rnTests.m
+++ ios/kkh_rnTests/kkh_rnTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface kkh_rnTests : XCTestCase
+@interface ASPATests : XCTestCase
 
 @end
 
-@implementation kkh_rnTests
+@implementation ASPATests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
